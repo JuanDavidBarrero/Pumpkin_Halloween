81127cd24c54d6da5cebcc9d08ab8157a27c9a40
JuanDavidBarrero <JuandBarrero@hotmail.com>
2020-10-07 17:37:55 -0500

    Color function added

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,35 @@
 #include <Arduino.h>
 
-void setup() {
-  
+#include <Adafruit_NeoPixel.h>
+
+#define PIN 12
+
+#define NUMPIXELS 3
+
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+void ColorPumpking(int, int, int, int);
+
+void setup()
+{
+  pixels.begin();
+  pixels.setBrightness(20);
 }
 
-void loop() {
-  
+void loop()
+{
+  ColorPumpking(5, 255, 50, 0);
+}
+void ColorPumpking(int times, int red, int green, int blue)
+{
+  pixels.clear();
+
+  for (int i = 0; i < NUMPIXELS; i++)
+  {
+    pixels.setPixelColor(i, pixels.Color(red, green, blue));
+
+    pixels.show();
+
+    delay(5);
+  }
 }
